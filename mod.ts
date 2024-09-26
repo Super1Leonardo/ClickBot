5fcfa0eca44dcf99a04bc3fe27d7380091069b18
SupLeonardo <levabaluevvv@gmail.com>
2024-09-26 21:26:06 +0500

    should work

--- mod.ts
+++ mod.ts
@@ -3,7 +3,7 @@ import { Bot } from "grammy"
 export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "")
 
 bot.command("start", async (ctx) => {
-    await ctx.reply("Привет! Отправь любую ссылку, и я верну её в коротком виде!\n\nРаботает на API clck.ru!")
+    await ctx.reply("Привет! Отправь любую ссылку, и я верну её в коротком виде!\n\nРаботает на API clck.ru")
 })
 
 bot.chatType("private").on("msg:text", async (ctx) => {
