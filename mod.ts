ba854ab6fd55fd5708791e8d2e3184c54e9a898b
mckoda09 <mckoda09@gmail.com>
2024-09-26 14:38:33 +0000

    all is good

---
+++ mod.ts
@@ -0,0 +1,14 @@
+import { Bot } from "grammy"
+
+export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "")
+
+bot.command("start", async (ctx) => {
+    await ctx.reply("Привет! Отправь любую ссылку, и я верну её в коротком виде!\n\nРаботает на API clck.ru")
+})
+
+bot.chatType("private").on("msg:text", async (ctx) => {
+    const url = ctx.message.text
+    const endpoint = "https://clck.ru/--?url="
+    const response = await fetch(endpoint + url)
+    await ctx.reply("Твоя ссылка:\n" + await response.text())
+})
