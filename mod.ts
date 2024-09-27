c0d81d078921f61c64dc4f0c46605615c2e45c39
levabaluevvv <levabaluevvv@gmail.com>
2024-09-27 17:58:20 +0000

    aa

--- mod.ts
+++ mod.ts
@@ -6,13 +6,13 @@ bot.command("start", async (ctx) => {
     await ctx.reply("Привет! Отправь любую ссылку, и я верну её в коротком виде!\n\nРаботает на API clck.ru")
 })
 
+bot.command("help", (ctx) => {
+    ctx.reply("No help")
+})
+
 bot.chatType("private").on("msg:text", async (ctx) => {
     const url = ctx.message.text
     const endpoint = "https://clck.ru/--?url="
     const response = await fetch(endpoint + url)
     await ctx.reply("Твоя ссылка:\n" + await response.text())
-})
-
-bot.command("help", (ctx) => {
-    ctx.reply("No help")
 })
